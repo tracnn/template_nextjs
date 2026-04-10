695e0f4a46254abadc755cbe7b2f978ea9d2c198
Nguyen Ngoc Trac <tracnn20021979@gmail.com>
2026-04-10 10:47:55 +0700

    feat: restore nav-item type and use-custom-table hook

---
+++ src/hooks/use-custom-table.ts
@@ -0,0 +1,60 @@
+import {
+  type MRT_TableOptions,
+  useMantineReactTable,
+} from "mantine-react-table";
+
+export type CustomTableOptions<TData extends Record<string, any> = {}> = Omit<
+  MRT_TableOptions<TData>,
+  | "manualPagination"
+  | "enablePagination"
+  | "mantinePaginationProps"
+  | "paginationDisplayMode"
+  | "mantineTableProps.align"
+  | "mantinePaperProps"
+  | "initialState.density"
+>;
+
+export const useCustomTable = <TData extends Record<string, any> = {}>(
+  tableOptions: CustomTableOptions<TData>,
+) => {
+  return useMantineReactTable({
+    ...{
+      paginationDisplayMode: "pages",
+      manualFiltering: true,
+      mantineTableProps: {
+        align: "center",
+      },
+      positionActionsColumn: "last",
+      mantinePaperProps: {
+        shadow: "0",
+        radius: "md",
+        p: "md",
+        withBorder: false,
+      },
+      displayColumnDefOptions: {
+        "mrt-row-actions": {
+          size: 200,
+        },
+      },
+      mantineFilterTextInputProps: {
+        style: { borderBottom: "unset", marginTop: "8px" },
+        variant: "filled",
+      },
+      mantineFilterSelectProps: {
+        style: { borderBottom: "unset", marginTop: "8px" },
+        variant: "filled",
+      },
+      enableColumnActions: false,
+      enableDensityToggle: false,
+      enableFullScreenToggle: false,
+      enableHiding: false,
+      enablePinning: false,
+      initialState: {
+        density: "md",
+      },
+      columns: [],
+      data: [],
+    },
+    ...tableOptions,
+  });
+};
---
+++ src/types/nav-item.ts
@@ -0,0 +1,10 @@
+import type { TablerIconsProps } from "@tabler/icons-react";
+
+export interface NavItem {
+  label: string;
+  icon: (props: TablerIconsProps) => JSX.Element;
+  link?: string;
+  initiallyOpened?: boolean;
+  links?: { label: string; link: string }[];
+  roles?: string[];
+}
